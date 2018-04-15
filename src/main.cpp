2e4ec5070051991c6021ba5fc646884986663fd6
Julio Turolla <julio.turolla@gmail.com>
2018-04-15 14:58:17 -0300

    add dht11

--- src/main.cpp
+++ src/main.cpp
@@ -5,14 +5,24 @@
  * then off for one second, repeatedly.
  */
 #include "Arduino.h"
+#include "DHT.h"
 
 #ifndef LED_BUILTIN
 #define LED_BUILTIN 13
 #endif
 
+#define DHTPIN 7
+#define DHTTYPE DHT11
+
+DHT dht(DHTPIN, DHTTYPE);
+
 void setup()
 {
   pinMode(LED_BUILTIN, OUTPUT);
+  Serial.begin(9600);
+  Serial.println("DHTxx test!");
+
+  dht.begin();
 }
 
 void loop()
@@ -26,6 +36,37 @@ void loop()
   // turn the LED off by making the voltage LOW
   digitalWrite(LED_BUILTIN, LOW);
 
-   // wait for a second
-  delay(1000);
+
+  // Reading temperature or humidity takes about 250 milliseconds!
+  // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+  float h = dht.readHumidity();
+  // Read temperature as Celsius (the default)
+  float t = dht.readTemperature();
+  // Read temperature as Fahrenheit (isFahrenheit = true)
+  float f = dht.readTemperature(true);
+
+  // Check if any reads failed and exit early (to try again).
+  if (isnan(h) || isnan(t) || isnan(f)) {
+    Serial.println("Failed to read from DHT sensor!");
+    return;
+  }
+
+  // Compute heat index in Fahrenheit (the default)
+  float hif = dht.computeHeatIndex(f, h);
+  // Compute heat index in Celsius (isFahreheit = false)
+  float hic = dht.computeHeatIndex(t, h, false);
+
+  Serial.print("Humidity: ");
+  Serial.print(h);
+  Serial.print(" %\t");
+  Serial.print("Temperature: ");
+  Serial.print(t);
+  Serial.print(" *C ");
+  Serial.print(f);
+  Serial.print(" *F\t");
+  Serial.print("Heat index: ");
+  Serial.print(hic);
+  Serial.print(" *C ");
+  Serial.print(hif);
+  Serial.println(" *F");
 }
